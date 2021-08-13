27ac547c977ef2d6e982cda412578910329ac46d
qiyang <qiyang@ustc.edu.cn>
2021-08-13 11:20:59 +0800

    format src

--- src/utils/PerspectiveMap.jl
+++ src/utils/PerspectiveMap.jl
@@ -1,30 +1,31 @@
 export PerspectiveMap3D
 
-function PerspectiveMap3D(kx::Real,ky::Real;rx=0.00001,ry=0.0001)
-	pmap(p) = (p[1]+kx*p[3]+rx*p[2],p[2]+ky*p[3]+ry*p[1])
-	return pmap
+function PerspectiveMap3D(kx::Real, ky::Real; rx = 0.00001, ry = 0.0001)
+    pmap(p) = (p[1] + kx * p[3] + rx * p[2], p[2] + ky * p[3] + ry * p[1])
+    return pmap
 end
 
 # rx ry make every lines not horizontal or vertical
-function PerspectiveMap3D(array::Array,kx::Real,ky::Real;rx=0.00001,ry=0.0001) 
-    tmap = PerspectiveMap3D(kx,ky;rx=rx,ry=ry)
+function PerspectiveMap3D(array::Array, kx::Real, ky::Real; rx = 0.00001, ry = 0.0001)
+    tmap = PerspectiveMap3D(kx, ky; rx = rx, ry = ry)
     N = size(array)[1]
-    new_locations = zeros(Float64,N,2)
-    for i =1:N
-        new_locations[i,:] .= tmap(array[i,:])
+    new_locations = zeros(Float64, N, 2)
+    for i = 1:N
+        new_locations[i, :] .= tmap(array[i, :])
     end
     return stretch2D!(new_locations)
 end
 
-PerspectiveMap3D(lattice::Lattice,kx::Real,ky::Real) = typeof(lattice)(lattice.graph,PerspectiveMap3D(lattice.locations,kx,ky))
+PerspectiveMap3D(lattice::Lattice, kx::Real, ky::Real) =
+    typeof(lattice)(lattice.graph, PerspectiveMap3D(lattice.locations, kx, ky))
 
-function stretch2D!(array::Array;xytol=0.1)
-    xmax = maximum(array[:,1])+xytol
-    xmin = minimum(array[:,1])-xytol
-    ymax = maximum(array[:,2])+xytol
-    ymin = minimum(array[:,2])-xytol
-    xspan,yspan = xmax-xmin,ymax-ymin
-    array[:,1] .= (array[:,1].-xmin)/xspan
-    array[:,2] .= (array[:,2].-ymin)/yspan
+function stretch2D!(array::Array; xytol = 0.1)
+    xmax = maximum(array[:, 1]) + xytol
+    xmin = minimum(array[:, 1]) - xytol
+    ymax = maximum(array[:, 2]) + xytol
+    ymin = minimum(array[:, 2]) - xytol
+    xspan, yspan = xmax - xmin, ymax - ymin
+    array[:, 1] .= (array[:, 1] .- xmin) / xspan
+    array[:, 2] .= (array[:, 2] .- ymin) / yspan
     return array
 end
--- src/utils/SwapGates.jl
+++ src/utils/SwapGates.jl
@@ -1,18 +1,21 @@
 export intersects2D
-function intersects2D(lattice2D::Lattice;eps=1E-10)
-    lines = [edgeline(lattice2D.locations,i) for i in edges(lattice2D.graph)]
-	nedges = ne(lattice2D.graph)
-	buffer = []
-	for i = 1:nedges-1
-		for j = i+1:nedges
-			isinter,point = intersects(lines[i],lines[j])
-			if isinter
-				tp = (lines[i][1],lines[i][2],lines[j][1],lines[j][2])
-				if norm(tp[1]-point)>eps && norm(tp[2]-point)>eps && norm(tp[3]-point)>eps && norm(tp[4]-point)>eps
-					push!(buffer,point)
-				end
-			end
-		end
-	end
-	return buffer
+function intersects2D(lattice2D::Lattice; eps = 1E-10)
+    lines = [edgeline(lattice2D.locations, i) for i in edges(lattice2D.graph)]
+    nedges = ne(lattice2D.graph)
+    buffer = []
+    for i = 1:nedges-1
+        for j = i+1:nedges
+            isinter, point = intersects(lines[i], lines[j])
+            if isinter
+                tp = (lines[i][1], lines[i][2], lines[j][1], lines[j][2])
+                if norm(tp[1] - point) > eps &&
+                   norm(tp[2] - point) > eps &&
+                   norm(tp[3] - point) > eps &&
+                   norm(tp[4] - point) > eps
+                    push!(buffer, point)
+                end
+            end
+        end
+    end
+    return buffer
 end
